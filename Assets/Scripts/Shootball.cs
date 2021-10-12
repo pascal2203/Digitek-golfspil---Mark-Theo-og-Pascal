4705c9a1bd45e97a332f1542c0b348636f3149dc
pascal2203 <75846575+pascal2203@users.noreply.github.com>
2021-10-12 10:53:36 +0200

    Shoot ball script virker ikke

--- Assets/Scripts/Shootball.cs
+++ Assets/Scripts/Shootball.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Shootball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float startPosx;
+    public float startPosz;
+    public float endPosx;
+    public float endPosz;
+    public float directionx;
+    public float directionz;
+    Rigidbody myRigidbody;
+    public float shootPower = 10f;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosz = Input.mousePosition.z;
+            startPosx = Input.mousePosition.x;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPosx = Input.mousePosition.x;
+            endPosz = Input.mousePosition.z;
+            directionx = startPosx - endPosx;
+            directionz = startPosz - endPosz;
+            myRigidbody.isKinematic = false;
+            myRigidbody.AddForce(directionx * shootPower, 0, directionz * shootPower);
+        }
     }
 }
